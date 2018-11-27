d555ad4e616e104b60956b71a856ca806473ba77
Matt Moore <matt.moore35@yahoo.co.uk>
2018-11-27 15:10:44 +0000

    Adding rasp pi code

---
+++ reader.py
@@ -0,0 +1,26 @@
+import serial
+import datetime
+ser = serial.Serial('/dev/ttyACM0', 9600)
+outf = open('output.txt', 'a')
+outf.truncate(0)
+
+def readValue(s):
+    try:
+        x = float(s[0])
+        y = float(s[1])
+        z = float(s[2])
+    except:
+        x = 0
+        y = 0
+        z = 0
+    return (x, y, z)
+    
+##dateStart = datetime.datetime.now()
+##finishTime = dateStart + datetime.timedelta(seconds=30)
+while (True):
+    read_serial = ser.readline()
+    s = read_serial.split()
+    (x, y, z) = readValue(s)
+    if (x != 0):
+        outf.write(str(x) + " " + str(y)+ " " + str(z) + "\n")
+        outf.flush()
